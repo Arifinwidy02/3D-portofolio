a5baac604137eeeff8736762bc2fb404512a047f
Arifinwidy02 <arifin@transfez.com>
2024-06-01 12:58:34 +0700

    fix: new tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
           to="/about"
+          target="_blank"
           className={({ isActive }) =>
             isActive ? "text-blue-500" : "text-black"
           }
